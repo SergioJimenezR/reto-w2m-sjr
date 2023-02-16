5a539f6e9f5c7397b032aaace8cb55fabd0dcab8
Sergio Jiménez Roncero <sergiojimenezroncero@gmail.com>
2023-02-16 02:08:49 +0100

    Inicialización del proyecto.

---
+++ src/main/java/com/w2m/sergiojimenez/retow2msjr/RetoW2mSjrApplication.java
@@ -0,0 +1,13 @@
+package com.w2m.sergiojimenez.retow2msjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoW2mSjrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoW2mSjrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w2m/sergiojimenez/retow2msjr/RetoW2mSjrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.sergiojimenez.retow2msjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoW2mSjrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
